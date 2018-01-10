64c9057ff8fc0f6dcadb59ed1826b70ef18c1b3c
Jorge Sanz <jorge.sanz@innovation4security.com>
2018-01-10 22:09:39 +0100

    gradle added to project

---
+++ band-manager/src/main/java/com/dersuzzala/bandwars/bandmanager/Greeter.java
@@ -0,0 +1,10 @@
+package com.dersuzzala.bandwars.bandmanager;
+
+/**
+ * Created by jorge on 10/01/18.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ band-manager/src/main/java/com/dersuzzala/bandwars/bandmanager/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.dersuzzala.bandwars.bandmanager;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Created by jorge on 10/01/18.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
